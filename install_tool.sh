87429ccbc04ccd4f706827950d4ae487ef3b0322
Joshua Smith <joshua.smith@campbellsci.com>
2021-06-22 15:14:10 -0600

    wrap graph wrangler in docker container and add install script

--- install_tool.sh
+++ install_tool.sh
@@ -27,8 +27,11 @@ source activate tensorflow_p37
 git submodule update --init --recursive
 docker build -t onnx-tf:latest -f Dockerfile.onnxtf .
 
+(cd GraphWrangler && docker build -t graph_wrangler:latest .)
+
 ln -s $PWD/onnx-tf /usr/local/bin/onnx-tf
 ln -s $PWD/verapak /usr/local/bin/verapak
+ln -s $PWD/GraphWrangler/graph_wrangler /usr/local/bin/graph_wrangler
 
 # install Verapak
 git clone https://github.com/formal-verification-research/ARFramework.git
